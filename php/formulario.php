542c3289696fc65b5037a2aa2fecb81fccc91bbb
Marcelo <marcelo.salar@gmail.com>
2020-07-29 23:24:32 -0300

    Se limpio el php de validación!

--- php/formulario.php
+++ php/formulario.php
@@ -1,38 +1,6 @@
 <?php
-
 echo '<pre>';
 
-function elementos(string $elemento, string $us): array
-{
-    return [
-        'nombre'        => "nombre de $us",
-        'ap_paterno'    => "apellido paterno",
-        'ap_materno'    => "apellido materno",
-        'alias'         => "alias del usuario",
-        'email'         => "correo electronico",
-        'nivel_acceso'  => "nivel de acceso",
-        'sexo'          => "sexo",
-        'edad'          => "edad",
-    ];
-}
-
-// var_dump(elementos("nombre", 'user'));
-// Read POST data
-// $data = json_decode(file_get_contents("h"));
-// exit();
-// var_dump($_REQUEST);
-// echo '<br>';
-
-
-$expresiones = [
-	'usuario' => '/^[a-zA-Z0-9\_\-]{4,16}$/', // Letras, numeros, guion y guion_bajo
-	'nombre' => '/^[a-zA-ZÀ-ÿ\s]{1,40}$/', // Letras y espacios, pueden llevar acentos.
-	'password' => '/^.{4,12}$/', // 4 a 12 digitos.
-	'correo' => '/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/',
-	'telefono' => '/^\d{7,14}$/', // 7 a 14 numeros.
-];
-// var_dump($expresiones);
-// echo '<br>';
 $campoEstado = [
 	'usuario' 	=> false,
 	'nombre' 	=> false,
@@ -53,18 +21,9 @@ $valores = [
   'telefono' 	=> ['number', 'required'],
   'terminos'    => ['radio', 'required']
 ];
-// 
-// $usuario 	= validarCampos($valores["usuario"], $_REQUEST['usuario']);
-// $nombre  	= validarCampos($valores["nombre"], $_REQUEST['nombre']);
-// $password  	= validarCampos($valores["password"], $_REQUEST['password']);
-// $password2 	= validarCampos($valores["password2"], $_REQUEST['password2']);
-// $correo  	= validarCampos($valores["correo"], $_REQUEST['correo']);
-// $telefono  	= validarCampos($valores["telefono"], $_REQUEST['telefono']);
-// $terminos  	= validarCampos($valores["terminos"], $_REQUEST['terminos']);
-// 
-
 /**
-* 
+* Clase Formulario
+* (donde nace hace la magia).
 */
 class Formulario
 {
@@ -146,107 +105,51 @@ class Formulario
 		var_dump($this->_mensaje);
 	}
 }
-
+// 
 $form = new Formulario($_REQUEST, $valores);
 $form->campo('usuario');
-// var_dump($form->validarCampo());
 $form->campo('nombre');
-// $form->validarCampo();
 $form->imprime();
 
-exit();
-// echo count($usuario);
-if (count($usuario) === 0) 
-	$campoEstado['usuario'] = true;
-else
-	var_dump($usuario);
+// 
+	if (count($usuario) === 0) 
+		$campoEstado['usuario'] = true;
+	else
+		var_dump($usuario);
 
-if (count($nombre) === 0) 
-	$campoEstado['nombre'] = true;
-else
-	var_dump($nombre);
+	if (count($nombre) === 0) 
+		$campoEstado['nombre'] = true;
+	else
+		var_dump($nombre);
 
-if (count($password) === 0) 
-	$campoEstado['password'] = true;
-else
-	var_dump($password);
+	if (count($password) === 0) 
+		$campoEstado['password'] = true;
+	else
+		var_dump($password);
 
-if (count($password2) === 0) 
-	$campoEstado['password2'] = true;
-else
-	var_dump($password2);
+	if (count($password2) === 0) 
+		$campoEstado['password2'] = true;
+	else
+		var_dump($password2);
 
-if (count($correo) === 0) 
-	$campoEstado['correo'] = true;
-else
-	var_dump($correo);
+	if (count($correo) === 0) 
+		$campoEstado['correo'] = true;
+	else
+		var_dump($correo);
 
-if (count($telefono) === 0) 
-	$campoEstado['telefono'] = true;
-else
-	var_dump($telefono);
+	if (count($telefono) === 0) 
+		$campoEstado['telefono'] = true;
+	else
+		var_dump($telefono);
 
-if (count($terminos) === 0) 
-	$campoEstado['terminos'] = true;
-else
-	var_dump($terminos);
-
-// var_dump($campoEstado);
-
+	if (count($terminos) === 0) 
+		$campoEstado['terminos'] = true;
+	else
+		var_dump($terminos);
+// verifico que todos los request sean true
+// esto tiene que ir en otra parte, pero estamos en desarrollo asi que
+// manos a la obra!!	
 if (!array_search(false, $campoEstado) ) 
 {
 	echo 'OK';
 }
-
-// Casos
-function validarCampos($valores, $campos)
-{
-	$mensaje = [];
-	if (isset($valores)) 
-	{	
-		foreach ($valores as $valor) 
-		{
-			// $mensaje = $valores[$valor];
-			switch ($valor) 
-			{
-		 		case 'text-con-numeros':
-	 				$patron = '/^[a-zA-Z0-9\_\-]{4,16}$/';
-		 			if (!preg_match($patron, $campos)) 
-		 	    		$mensaje[] = 'El campo requiere solo texto y números  '.$campos.'';
-				break;
-				case 'text':
-	 				$patron = '/^[a-zA-ZÀ-ÿ\s]{1,40}$/';
-		 			if (!preg_match($patron, $campos)) 
-		 	    		$mensaje[] = 'El campo requiere solo texto '.$campos.'';
-				break;
-				case 'required':
-					if ($campos == "") 
-		 	    		$mensaje[] = 'es requerido '.$campos.'';
-				break;
-				case 'password':
-					$mensaje[] = 'es nombre '.$campos.'';
-				break;
-				case 'number':
-					if ($campos == "") 
-		 	    		$mensaje[] = 'es number '.$campos.'';
-				break;
-				case 'radio':
-					if ($campos == "") 
-						$mensaje[] = 'El campo requiere solo radio '.$campos.'';
-				break;
-				case 'email':
-					$patron = '/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/';
-					if (!preg_match($patron, $campos)) 
-		 	    		$mensaje[] = 'El campo email requiere  '.$campos.'';
-				break;
-				default:
-					$mensaje[] = 'no es ninguno '.$campos.'';
-				break;
-			}
-		}	
-	}
-	else
-		$mensaje = "El registro nombre no existe";
-	return  $mensaje;
-}
-
